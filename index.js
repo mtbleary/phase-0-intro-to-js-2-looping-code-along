3913ffa4d2ec92034da8012fc9149902516445a5
matthew leary <matt-leary@live.co.uk>
2022-04-30 12:02:52 +0100

    commit tree ded5739908e54b764ddb2413a50c86ec32f54dca

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+const thankYous = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(thankYous) {
+  for (let i = 0; i < thankYous.length; i++) {
+    console.log(`Thank you, ${thankYous[i]}, for the wonderful surprise gift!`);
+  }
+
+  return thankYous;
+}
+
+writeCards(thankYous);
+
+
+  const address = {
+    street1: '11 Broadway',
+    street2: '2nd Floor',
+    city: 'New York',
+    state: 'NY',
+    zipCode: "10004"
+  };
+  
+  for (const key in address) {
+    console.log(address[key]);
+  }
+
+  const addressOne = {
+    street1: '11 Broadway',
+    street2: '2nd Floor',
+    city: 'New York',
+    state: 'NY',
+    zipCode: '10004'
+  };
+  
+  for (const key in addressOne) {
+    console.log(addressOne.street1);
+  }
